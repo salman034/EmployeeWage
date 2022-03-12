b8ee874811148498898fe12cdd1662692e3a2c1f
unknown <you@example.com>
2022-03-12 16:13:02 +0530

    Added: Checking Employee Status

--- src/com/bl/employeeWages/EmployeeWages.java
+++
@@ -1,7 +0,0 @@
-package com.bl.employeeWages;
-
-public class EmployeeWages {
-    public static void main(String[] args) {
-        System.out.println("Welcom to Employee Wage problems");
-    }
-}
---
+++ src/employeeWage/EmployeeWages.java
@@ -0,0 +1,12 @@
+package employeeWage;
+
+public class EmployeeWages {
+    public static void main(String[] args) {
+        int Full_Time = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == Full_Time)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+    }
+}

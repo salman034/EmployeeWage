3a16c6c48f9a61611b3d9e1f87a6b0489d5ab89a
unknown <you@example.com>
2022-03-12 15:03:06 +0530

    Added: Welcome To Employee Wage Problems

---
+++ src/com/bl/employeeWages/EmployeeWages.java
@@ -0,0 +1,7 @@
+package com.bl.employeeWages;
+
+public class EmployeeWages {
+    public static void main(String[] args) {
+        System.out.println("Welcom to Employee Wage problems");
+    }
+}
